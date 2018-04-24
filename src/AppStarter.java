00b384fbe8d83b5d853f490292ebeee3f9d07b67
Grimm, Tobias <master_11114@web.de>
2018-04-24 11:19:43 +0200

    Added Worker, Server and Client classes

--- src/AppStarter.java
+++ src/AppStarter.java
@@ -1,6 +1,21 @@
 public class AppStarter {
 
+    private Client client;
+    private Server server;
+    private Worker worker;
+
     public static void main(String[] args) {
+        switch(args[0]){
+            case "client":
+                //Start Client
+                break;
+            case "server":
+                //Start server
+                break;
+            case "worker":
+                //Start worker
+                break;
+        }
         System.out.println("Test Test");
     }
 }
